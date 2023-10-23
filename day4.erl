0bdbdf8cae38edc3fad14256a770099df9bd446a
ljh <junhyk2@twinny.co.kr>
2023-10-24 00:53:41 +0900

    20231024-day4-1

--- day4.erl
+++ day4.erl
@@ -2,15 +2,22 @@
 
 -author("jjm").
 
--export([is_power_of_four/1]).
+-export([is_power_of_four/1, is_power_of_four2/1]).
 
 -spec is_power_of_four(N :: integer()) -> boolean().
 is_power_of_four(1) ->
-  true;  
+  true;
 is_power_of_four(N) when N =< 0 ->
   false;
 is_power_of_four(N) ->
   case N rem 4 of
-    0 -> is_power_of_four(N div 4);
-    _ -> false
-end.
+    0 ->
+      is_power_of_four(N div 4);
+    _ ->
+      false
+  end.
+
+-spec is_power_of_four2(N :: integer()) -> boolean().
+is_power_of_four2(N) ->
+  % (N > 0)  and (N band 16#5555_5555 == N).
+  (N > 0) and (N band (N - 1) == 0) and (N band 16#5555_5555 == N).
