354d7bccf4750f4aaab8151690a338e31cfe5076
ljh <junhyk2@twinny.co.kr>
2023-10-31 02:17:12 +0900

    20231030-day1_0_458

--- day1_0.erl
+++ day1_0.erl
@@ -2,7 +2,7 @@
 
 -author("jjm").
 
--export([sort_by_bits/1]).
+-export([sort_by_bits/1, poor_pigs/3]).
 
 -spec sort_by_bits(Arr :: [integer()]) -> [integer()].
 sort_by_bits(Arr) ->
@@ -17,7 +17,7 @@ solution([], _, Result) ->
 solution(T, N, Result) ->
     % io:format("solution ~p  ~n", [T]),
     Fun = fun(XX) ->
-            %  io:format("integer_to_list(XX, 2) ~p  ~n", [integer_to_list(XX, 2)]),
+             %  io:format("integer_to_list(XX, 2) ~p  ~n", [integer_to_list(XX, 2)]),
              %  io:format("L ~p  ~n", [L]),
              L = lists:map(fun(X) -> X - 48 end, integer_to_list(XX, 2)),
              lists:sum(L)
@@ -25,3 +25,24 @@ solution(T, N, Result) ->
     T1 = [X || X <- T, Fun(X) == N],
     % io:format("solution1 ~p  ~n", [T]),
     solution(lists:subtract(T, T1), N + 1, Result ++ T1).
+    
+% https://leetcode.com/problems/poor-pigs/solutions/94278/Very-Clear-Explanation-by-Short-Examples/
+-spec poor_pigs(Buckets :: integer(),
+                MinutesToDie :: integer(),
+                MinutesToTest :: integer()) ->
+                   integer().
+poor_pigs(N, _, _) when N < 2 ->
+    0;
+poor_pigs(N, MinutesToDie, MinutesToTest) ->
+    Count = MinutesToTest div MinutesToDie,
+    io:format("Count ~p  ~n", [Count]),
+    PigCount = pig_solution(N, 1, Count),
+    PigCount.
+
+pig_solution(BucketsCount, Pig, Count) ->
+    case BucketsCount =< math:pow(Count + 1, Pig) of
+        true ->
+            Pig;
+        _ ->
+            pig_solution(BucketsCount, Pig + 1, Count)
+    end.
